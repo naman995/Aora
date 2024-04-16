e1131f7dc3fc611c2bf699a0f131617e95f34cc9
naman995 <rajputnaman823@gmail.com>
2024-04-16 19:48:05 +0530

    Onboarding screen done

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,55 @@
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants/'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-
+import { Redirect, router } from 'expo-router';
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="text-3xl bg-red-200 font-pblack">Aora</Text>
-            <StatusBar style="auto" />
-            <Link href="/home">Go To Home</Link>
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: '100 %' }} >
+                <View className='w-full items-center h-full px-4'>
+                    <Image
+                        source={images.logo}
+                        className='w-[130px] h-[84px] '
+                        resizeMode='contain'
+                    />
+                    <Image
+                        source={images.cards}
+                        className='max-w-[380px] w-full h-[300px] '
+                        resizeMode='contain'
+                    />
+                    <View className='relative mt-5'>
+                        <Text className='text-3xl text-white font-bold text-center'>Discover Endless Possibilities with
+                            <Text className='text-secondary-200'>Aora</Text></Text>
+                        <Image
+                            source={images.path}
+                            className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+                            resizeMode='contain' />
+                    </View>
+                    <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where Creativity meets
+                        innovation : embark on a journey of limitless
+                        exploration with Aora</Text>
+                    <CustomButton
+                        title="Continue With Email"
+                        handlePress={() => router.push('/sign-in')}
+                        containerStyles="w-full mt-7" />
+                </View>
+            </ScrollView>
+            <StatusBar backgroundColor='#161622' style='light' />
+        </SafeAreaView>
     );
 }
 
- 
+const CustomButton = ({ title, handlePress, containerStyles, texStyles, isLoading }) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            // style={[containerStyle, { backgroundColor: '#FFC700' }]} 
+            className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center 
+            ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}>
+            <Text className={`text-primary text-lg font-psemibold`}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
