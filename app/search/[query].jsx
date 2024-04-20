af21fa070bbef08df26e3f08928bfe216513bd9c
naman995 <rajputnaman823@gmail.com>
2024-04-20 14:02:53 +0530

    Update search functionality and layout components

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useState, useEffect } from 'react'
+
+import { getAllPosts, getLatestPosts, searchPosts } from "../../lib/appwrite";
+import { useAppwrite } from "../../lib/useAppwrite";
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import VideoCard from '../../components/VideoCard'
+import { useLocalSearchParams } from 'expo-router';
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(()=> searchPosts(query));
+
+  useEffect(() => {
+    refetch()
+  }, [query])
+
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        // data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => {
+          return (
+            <View className="my-6 px-4">
+              <Text className="font-pmedium text-sm text-gray-100">Search Results</Text>
+              <Text className="text-2xl font-psemibold text-white">{query}</Text>
+              <View className='mt-6 mb-8'>
+                <SearchInput initialQuery={query} />
+              </View>
+            </View>
+          )
+        }
+        }
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No Videos Found for the search query. "
+          />
+        )}
+
+      />
+    </SafeAreaView>
   )
 }
 
 export default Search
-
-const styles = StyleSheet.create({})
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,34 +1,39 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, TouchableOpacity, Image, Alert } from 'react-native'
 import React from 'react'
 import { useState } from 'react'
 import { icons } from '../constants'
+import { usePathname } from 'expo-router'
+import { router } from 'expo-router'
 
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
-    const [showPassword, setShowPassword] = useState(false)
+const SearchInput = ({ initialQuery }) => {
+    const pathname = usePathname();
+    const [query, setQuery] = useState(initialQuery || '');
     return (
-
-        <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl
-            focus:border-secondary   items-center flex-row space-x-4">
+        <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
             <TextInput
-                className="flex-1 text-white  mt-0.5 text-base font-pregular"
-                value={value}
-                placeholder={placeholder}
-                placeholderTextColor="#7B7B8B"
-                onChangeText={handleChangeText}
-                secureTextEntry={title === "Password" && !showPassword}
-                {...props}
+                className="text-base mt-0.5 text-white flex-1 font-pregular"
+                value={query}
+                placeholder="Search for Videos"
+                placeholderTextColor="#CDCDE0"
+                onChangeText={(e) => setQuery(e)}
             />
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    if (!query) {
+                        return Alert.alert('Please enter a search query')
+                    }
+                    if (pathname.startsWith('/search')) {
+                        router.setParams({ query })
+                    }
+                    else router.push(`/search/${query}`)
+                }}>
                 <Image
                     source={icons.search}
-                    className="w-6 h-6"
+                    className="w-5 h-5 "
                     resizeMode="contain"
                 />
-
-
             </TouchableOpacity>
-
-        </View>
+        </View >
     )
 }
 
