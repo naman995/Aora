fa4fb66d61c64f006425735e775d46903fe63084
naman995 <rajputnaman823@gmail.com>
2024-04-20 07:11:26 +0530

    Update home screen with new components and functionality

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,8 +1,8 @@
 import { Text, View, FlatList, Image, RefreshControl } from 'react-native'
 import React, { useState, useEffect } from 'react'
- 
-import { getAllPosts } from "../../lib/appwrite";
-import { useAppwrite } from "../../lib/useAppwrite";
+
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import { useAppwrite   } from "../../lib/useAppwrite";
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
@@ -12,6 +12,7 @@ import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
 
   const [refreshing, setRefreshing] = useState(false);
@@ -47,12 +48,12 @@ const Home = () => {
                 </View>
               </View>
               <SearchInput placeholder={"Search For a Video Topic"} />
-              {/* <View className="w-full flex-1 pt-5 pb-8">
+              <View className="w-full flex-1 pt-5 pb-8">
                 <Text className="text-gray-100 text-lg font-pregular mb-3">
                   Latest Video
                 </Text>
-                <Trending posts={[{ id: 1 }, { id: 2 }] ?? []} />
-              </View> */}
+                <Trending posts={latestPosts ?? []} />
+              </View>
             </View>
           )
         }
